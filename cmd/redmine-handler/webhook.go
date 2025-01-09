d4a059dec53f9f2c84147d53fabad5c418061ebc
Max Ignatenko <gelraen.ua@gmail.com>
2025-01-09 17:00:59 +0000

    feature: webhook handler metrics

--- cmd/redmine-handler/webhook.go
+++ cmd/redmine-handler/webhook.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"sync/atomic"
+	"time"
 
 	"github.com/Jille/convreq"
 	"github.com/Jille/convreq/respond"
@@ -12,6 +14,7 @@ import (
 	"github.com/rs/zerolog"
 
 	"bsky.watch/modkit/pkg/config"
+	"bsky.watch/modkit/pkg/metrics"
 	"github.com/bluesky-social/indigo/xrpc"
 )
 
@@ -50,16 +53,27 @@ func (h *handler) HandleWebhook(ctx context.Context, req *http.Request) convreq.
 	log := zerolog.Ctx(ctx).With().Uint64("request_id", requestId.Add(1)).Logger()
 	ctx = log.WithContext(ctx)
 
+	updateMetrics := func(success bool, statusCode int, start time.Time) {
+		duration := time.Since(start).Seconds()
+		metrics.RequestStatus.WithLabelValues("redmine-webhook", fmt.Sprint(success), fmt.Sprint(statusCode)).Inc()
+		metrics.RequestDuration.WithLabelValues("redmine-webhook", fmt.Sprint(success), fmt.Sprint(statusCode)).Add(duration)
+		metrics.RequestStats.WithLabelValues("redmine-webhook", fmt.Sprint(success)).Observe(duration)
+	}
+
 	defer req.Body.Close()
+
+	start := time.Now()
 
 	var fullPayload interface{}
 	if err := json.NewDecoder(req.Body).Decode(&fullPayload); err != nil {
 		log.Error().Err(err).Msgf("Failed to parse the payload: %s", err)
+		updateMetrics(false, http.StatusBadRequest, start)
 		return respond.BadRequest("bad JSON")
 	}
 
 	b, err := json.MarshalIndent(fullPayload, "", "  ")
 	if err != nil {
+		updateMetrics(false, http.StatusInternalServerError, start)
 		return respond.InternalServerError("failed to serialize")
 	}
 
@@ -69,15 +83,18 @@ func (h *handler) HandleWebhook(ctx context.Context, req *http.Request) convreq.
 
 	if err := json.Unmarshal(b, payload); err != nil {
 		log.Error().Err(err).Msgf("Failed to parse payload: %s", err)
+		updateMetrics(false, http.StatusBadRequest, start)
 		return respond.BadRequest("bad payload")
 	}
 
 	err = h.processPayload(ctx, &payload.Payload)
 	if err != nil {
 		log.Error().Err(err).Msgf("Processing failed: %s", err)
+		updateMetrics(false, http.StatusBadGateway, start)
 		return respond.BadGateway("error")
 	}
 
+	updateMetrics(true, http.StatusOK, start)
 	return respond.String("OK")
 }
 
